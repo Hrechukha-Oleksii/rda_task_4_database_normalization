61a6ac9d475bb8868ab284106f98df6e3622fcb7
Hrechukha-Oleksii <Oleksii_Hrechukha@outlook.com>
2026-07-29 11:26:05 +0300

    Solution2

--- task.sql
+++ task.sql
@@ -13,17 +13,16 @@ CREATE TABLE WarehouseInfo(
     ID INT,
     Name VARCHAR(50),
     Address VARCHAR(50),
+    Amount INT,
     PRIMARY KEY (ID)
 );
 
 CREATE TABLE ProductInventory (
     ID INT,
     ProductName VARCHAR(50),
-    WarehouseAmount INT,
     WarehouseID INT,
     FOREIGN KEY (WarehouseID) REFERENCES WarehouseInfo(ID) ON DELETE NO ACTION,
     CountryID INT,
-	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
     PRIMARY KEY (ID)
 );
 
@@ -34,11 +33,11 @@ INSERT INTO Countries (ID,Name)
 INSERT INTO Countries (ID,Name)
 	VALUES (2, 'Country2');
 
-INSERT INTO WarehouseInfo (ID,Name,Address) 
-    VALUES (1, 'Warehouse-1', 'City-1, Street-1'),
-    (2, 'Warehouse-2', 'City-2, Street-2');
+INSERT INTO WarehouseInfo (ID,Name,Address, Amount) 
+    VALUES (1, 'Warehouse-1', 'City-1, Street-1', 2),
+    (2, 'Warehouse-2', 'City-2, Street-2', 5);
     
 INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseID,CountryID)
-	VALUES (1, 'AwersomeProduct', 2, 1, 1);
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseID,CountryID)
-	VALUES (2, 'AwersomeProduct', 5, 2, 2);
+	VALUES (1, 'AwersomeProduct', 1, 1);
+INSERT INTO ProductInventory (ID,ProductName,WarehouseID,CountryID)
+	VALUES (2, 'AwersomeProduct', 2, 2);
