3bfffa213a282ac7490d5ba3b89d7706bc18548f
Hrechukha-Oleksii <Oleksii_Hrechukha@outlook.com>
2026-07-29 13:26:36 +0300

    Solution4

--- task.sql
+++ task.sql
@@ -9,6 +9,12 @@ CREATE TABLE Countries (
     PRIMARY KEY (ID)
 );
 
+CREATE TABLE WarehouseAmount(
+    ID INT,
+    ProductAmount INT,
+    PRIMARY KEY (ID)
+);
+
 CREATE TABLE WarehouseInfo(
     ID INT,
     Name VARCHAR(50),
@@ -17,12 +23,6 @@ CREATE TABLE WarehouseInfo(
     FOREIGN KEY (WarehouseAmountID) REFERENCES WarehouseAmount(ID) ON DELETE NO ACTION,
     PRIMARY KEY (ID)
 );
-
-CREATE TABLE WarehouseAmount(
-    ID INT,
-    ProductAmount INT,
-    PRIMARY KEY (ID)
-)
 
 CREATE TABLE ProductInventory (
     ID INT,
@@ -47,7 +47,7 @@ INSERT INTO WarehouseInfo (ID,Name,Address, WarehouseAmountID)
     VALUES (1, 'Warehouse-1', 'City-1, Street-1', 1),
     (2, 'Warehouse-2', 'City-2, Street-2', 2);
     
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseID,CountryID)
+INSERT INTO ProductInventory (ID,ProductName,WarehouseID,CountryID)
 	VALUES (1, 'AwersomeProduct', 1, 1);
 INSERT INTO ProductInventory (ID,ProductName,WarehouseID,CountryID)
 	VALUES (2, 'AwersomeProduct', 2, 2);
